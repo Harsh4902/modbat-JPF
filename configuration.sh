e450c79965f02c4ce686acb386196ae988208086
Harsh4902 <harshparmar4902@gmail.com>
2024-06-13 11:42:42 +0000

    configuration.sh added

---
+++ configuration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#basic setup for all libraries
+
+[ -e "${SCALA_HOME}" ] || SCALA_HOME="/usr/share/scala/"
+
+[ -e "${JPF_HOME}" ] || JPF_HOME="${HOME}/jpf/jpf-core/"
+
+[ -e "${MODBAT_HOME}" ] || MODBAT_HOME="${HOME}/jpf/modbat/"
+
+[ -e "${JPF_NHANDLER_HOME}" ] || JPF_NHANDLER_HOME="${HOME}/jpf/jpf-nhandler/"
+
+#creating .jpf configuration file for running modbat inside jpf
+
+echo "@using = jpf-nhandler" > $HOME/jpf/ModbatTest.jpf
+echo "target = modbat.mbt.Main" >> $HOME/jpf/ModbatTest.jpf
+echo "classpath = $SCALA_HOME/lib/scala-library.jar:$MODBAT_HOME/build/modbat-3.5.jar:$JPF_NHADLER_HOME/build/jpf-nhadler.jar" >> $HOME/jpf/ModbatTest.jpf
+echo "target.args = $1" >> $HOME/jpf/ModbatTest.jpf
+
