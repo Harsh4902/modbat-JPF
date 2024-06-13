5c30137ea7b8a9ad1091ddb9aae7ae5dd3f0d003
Cyrille Artho <artho@kth.se>
2024-06-13 13:51:36 +0200

    Fix MODBAT_HOME for users where it is in ~/modbat, fix classpath.

--- configuration.sh
+++ configuration.sh
@@ -10,12 +10,14 @@
 
 [ -e "${MODBAT_HOME}" ] || MODBAT_HOME="${HOME}/jpf/modbat/"
 
+[ -e "${MODBAT_HOME}" ] || MODBAT_HOME="${HOME}/modbat/"
+
 [ -e "${JPF_NHANDLER_HOME}" ] || JPF_NHANDLER_HOME="${HOME}/jpf/jpf-nhandler/"
 
 #creating .jpf configuration file for running modbat inside jpf
 
 echo "@using = jpf-nhandler" > $HOME/jpf/ModbatTest.jpf
 echo "target = modbat.mbt.Main" >> $HOME/jpf/ModbatTest.jpf
-echo "classpath = $SCALA_HOME/lib/scala-library.jar:$MODBAT_HOME/build/modbat-3.5.jar:$JPF_NHADLER_HOME/build/jpf-nhadler.jar" >> $HOME/jpf/ModbatTest.jpf
+echo "classpath = $SCALA_HOME/lib/scala-library.jar:$MODBAT_HOME/build/modbat.jar:$JPF_NHADLER_HOME/build/jpf-nhandler.jar" >> $HOME/jpf/ModbatTest.jpf
 echo "target.args = $1" >> $HOME/jpf/ModbatTest.jpf
 
