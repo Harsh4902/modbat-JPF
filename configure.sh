1191765e5ddc4149eb899bd98720850cec575997
Cyrille Artho <artho@kth.se>
2024-06-20 13:12:39 +0200

    configure.sh: Use -d instead of -e, add comments.

--- configure.sh
+++ configure.sh
@@ -2,17 +2,22 @@
 
 #basic setup for all libraries
 
-[ -e "${SCALA_HOME}" ] || SCALA_HOME="/usr/share/scala"
+[ -d "${SCALA_HOME}" ] || SCALA_HOME="/usr/share/scala"
+# 1. SCALA_HOME is not set (default)
+# 2. the file at ${SCALA_HOME} does not exist
+# 3. SCALA_HOME is then set to /usr/share/scala
 
-#[ -e "${SCALA_HOME}" ] || SCALA_HOME="`which scala | sed -e 's,/bin/scala,,'`"
+[ -d "${SCALA_HOME}" ] || SCALA_HOME="`which scala | sed -e 's,/bin/scala,,'`"
+# Ubuntu: 1. there is a /usr/share/scala, skip the rest
+# Mac Ports: 2. right-hand side finds the right location
 
-[ -e "${JPF_HOME}" ] || JPF_HOME="/tmp/m-jpf/jpf-core"
+[ -d "${JPF_HOME}" ] || JPF_HOME="/tmp/m-jpf/jpf-core"
 
-[ -e "${MODBAT_HOME}" ] || MODBAT_HOME="/tmp/m-jpf/modbat"
+[ -d "${MODBAT_HOME}" ] || MODBAT_HOME="/tmp/m-jpf/modbat"
 
-#[ -e "${MODBAT_HOME}" ] || MODBAT_HOME="${HOME}/modbat/"
+[ -d "${MODBAT_HOME}" ] || MODBAT_HOME="${HOME}/modbat/"
 
-[ -e "${JPF_NHANDLER_HOME}" ] || JPF_NHANDLER_HOME="/tmp/m-jpf/jpf-nhandler"
+[ -d "${JPF_NHANDLER_HOME}" ] || JPF_NHANDLER_HOME="/tmp/m-jpf/jpf-nhandler"
 
 #creating .jpf configuration file for running modbat inside jpf
 
